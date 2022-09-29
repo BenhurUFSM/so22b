87c96e7b8617621b290276595be18aeb155cd11b
Crazynds <luli.broch@gmail.com>
2022-09-29 14:47:06 -0300

    feat: criado um ex para demonstrar uso de pilha

---
+++ t0/ex3.asm
@@ -0,0 +1,13 @@
+     CARGI 128
+loop PUSHA
+     RESTO dez
+     ESCR 0
+     POPA
+     DIV dez
+     CMPA zero
+     DESVE fim
+     DESV loop
+fim  PARA         ; stop
+
+dez  VALOR 10
+zero VALOR 0
